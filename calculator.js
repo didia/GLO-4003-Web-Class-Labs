ef4d09fec838a25dcc94766642336a8c8ab4bf59
Aristotte Diasonama <aristotediasonama@yahoo.fr>
2015-10-03 14:12:02 -0400

    Laboratoire 4

--- calculator.js
+++ calculator.js
@@ -1,209 +1,116 @@
+var str = "12/5*9+9.4*2".replace(/[^-()\d/*+.]/g, '');
 
-var Calculator = function() {
-    this.lastResult = null;
-    this.storedNumber = null;
-
-    this.reinitialize = function() {
-        this.lastResult = null;
-    };
-
-    this.getResult = function(){
-        return this.lastResult;
-    };
-
-    this.storeNumber = function(number) {
-        verifyNumber(number);
-        this.storedNumber = number;
+var f = [];
+Math.factorial = function (n) {
+    if (n == 0 || n == 1) {
+        return 1;
     }
-
-    this.getStoredNumber = function() {
-        return this.storedNumber;
+    if (f[n] > 0) {
+        return f[n];
     }
+    return f[n] = Math.factorial(n - 1) * n;
+};
 
-    this.add = function() {
-        var result = 0;
-        if (this.lastResult != null) {
-            result = this.lastResult;
+// Classe Calculator
+// Toutes les méthodes sauf `equals` retournent `this`, ce qui permet de chainer les appels
+// Ex: 
+// var calculator = new Calculator()
+// calculator.add(2).add(3).subtract(2).equals()
+// Retourne : 2 
+// 2 + 3 - 2 = 2
+var Calculator = function () {
+    var memory;
+
+    var equation = '';
+
+    // Ajouter seulement une valeur à l'équation
+    // Sera utile pour lorsque cette classe sera connectée au UI
+    this.value = function (value) {
+        if (typeof value !== 'undefined') {
+            equation += parseFloat(value);
         }
-        for(var i = 0; i < arguments.length; i++) {
-            verifyNumber(arguments[i]);
-            result += arguments[i];
-        }
-        this.lastResult = result;
         return this;
     };
 
-    this.substract = function() {
-        var startingIndex = 0;
-        if(this.lastResult === null & arguments.length >= 1) {
-            verifyNumber(arguments[0]);
-            result = arguments[0];
-            startingIndex = 1;
-        }
-        else if (this.lastResult != null) {
-            result = this.lastResult;
-        }
-        else {
-            result = 0;
-        }
-
-        for(var i = startingIndex; i < arguments.length; i++) {
-            verifyNumber(arguments[i]);
-            result -= arguments[i];
-        }
-
-        this.lastResult = result;
+    // Réinitialiser l'équation 
+    this.clear = function () {
+        equation = '';
         return this;
     };
 
-    this.multiply = function() {
-        var result = 1;
-        if(this.lastResult != null) {
-            result = this.lastResult;
+    this.add = function (value) {
+        equation += '+';
+        if (typeof value !== 'undefined') {
+            equation += parseFloat(value);
         }
-        for(var i = 0; i < arguments.length; i++) {
-            verifyNumber(arguments[i]);
-            result *= arguments[i];
-        }
-        this.lastResult = result;
         return this;
     };
 
-    this.divide = function() {
-        var startingIndex = 0;
-        if(this.lastResult === null & arguments.length >= 1) {
-            verifyNumber(arguments[0]);
-            result = arguments[0];
-            startingIndex = 1;
+    this.subtract = function (value) {
+        equation += '-';
+        if (typeof value !== 'undefined') {
+            equation += parseFloat(value);
         }
-        else if (this.lastResult != null) {
-            result = this.lastResult;
-        }
-        else {
-            result = 0;
-        }
-
-        for(var i = startingIndex; i < arguments.length; i++) {
-            verifyNumber(arguments[i]);
-            result /= arguments[i];
-        }
-        this.lastResult = result;
         return this;
     };
 
-    this.factorial = function(number) {
-        if(shouldUseLastResult(this.lastResult, number)){
-            number = this.lastResult;
+    this.multiply = function (value) {
+        equation += '*';
+        if (typeof value !== 'undefined') {
+            equation += parseFloat(value);
         }
-        if(number < 0) {
-            throw "Number passed to factoriel should be positive";
-        }
-        this.lastResult = doFactorial(number);
         return this;
-        
+    };
+
+    this.divide = function (value) {
+        equation += '/';
+        if (typeof value !== 'undefined') {
+            if (value == 0) {
+                throw "Division par zéro!!!";
+            }
+            equation += parseFloat(value);
+        }
+        return this;
+    };
+
+    this.sin = function (value) {
+        equation += 'Math.sin(' + parseFloat(value) + ')';
+        return this;
+    };
+
+    this.cos = function (value) {
+        equation += 'Math.cos(' + parseFloat(value) + ')';
+        return this;
+    };
+
+    this.tan = function () {
+        equation += 'Math.tan(' + parseFloat(value) + ')';
+        return this;
+    };
+
+    this.setMemory = function (memoryValue) {
+        memory = memoryValue;
+    };
+
+    this.getMemory = function () {
+        return memory;
+    };
+
+    this.factorial = function (value) {
+        if (typeof value === 'undefined') {
+            equation = 'Math.factorial(' + this.equals() + ')'
+        } else {
+            equation += 'Math.factorial(' + parseFloat(value) + ')'
+        }
+        return this;
+    };
+
+    this.equals = function () {
+        // Il faut être très prudent avec eval !!! Eval pourrait permettre d'injecter du code malicieux et l'exécuter
+        // C'est pourquoi toutes nos variables 'value' sont passées dans 'parseFloat'
+        console.log('Evaluating :', equation);
+        var equationSolution = eval(equation);
+        equation = '';
+        return equationSolution;
     }
-
-    this.sin = function(angle) {
-        
-        if(shouldUseLastResult(this.lastResult, angle)) {
-           angle = this.lastResult;
-        }
-        else {
-            verifyNumber(angle);
-        }
-
-        return Math.sin(angle);
-        
-    }
-
-    this.cos = function(angle) {
-        if(shouldUseLastResult(this.lastResult, angle)) {
-            angle = this.lastResult;
-        }
-        else {
-            verifyNumber(angle);
-        }
-
-        return Math.cos(angle);
-    }
-
-    this.tan = function(angle) {
-        if(shouldUseLastResult(this.lastResult, angle)) {
-            angle = this.lastResult;
-        }
-        else {
-            verifyNumber(angle);
-        }
-
-        return Math.tan(angle);
-    }
-
-    function verifyNumber(input) {
-        if(typeof(input) != "number") {
-            throw "Input to calculator operations should be numbers";
-        }
-    }
-
-    function shouldUseLastResult(lastResult, input) {
-        return lastResult != null & (input === null || typeof(input) === "undefined")
-    }
-
-    function doFactorial(number) {
-        if(number === 0 || number === 1) {
-            return 1;
-        }
-        else {
-            return number * doFactorial(number - 1);
-        }
-    }
-
-    function sin(angle) {
-        return Math.sin(angle);
-    }
-
-}
-
-function getRandomInteger() {
-    return Math.floor(Math.random() * 100); 
-}
-
-var a = getRandomInteger();
-var b = getRandomInteger();
-var c = getRandomInteger();
-var d = getRandomInteger();
-var e = getRandomInteger();
-var calculator = new Calculator();
-
-console.log("Executing ", a , " + ",  b, " + ",  c, " + ", d, " + ", e);
-console.log("Result : ", calculator.add(a, b, c, d, e).getResult());
-
-console.log("Executing substract ", b, " from last result");
-console.log("Result: ", calculator.substract(b).getResult());
-
-console.log("Reinitialize calculator and execute multiplication: ", b, " * ", d)
-calculator.reinitialize();
-console.log("Result: ", calculator.multiply(b, d).getResult());
-
-console.log("Divide last result by : ", d);
-console.log("Result: ", calculator.divide(d).getResult()); 
-
-console.log("Now let do some crazy Chaining: ", a, "+", b, "-", c, "*", d, "/" ,e, " :D ")
-console.log("Note: Operations are executed in order i.e not by operations priority")
-calculator.reinitialize();
-console.log("Result: ", calculator.add(a,b).substract(c).multiply(d).divide(e).getResult())
-
-console.log("Now execute this: sin(", b, "!)")
-calculator.reinitialize();
-console.log("Result: ", calculator.factorial(b).sin());
-
-calculator.reinitialize();
-console.log("Now save number: ", c);
-calculator.storeNumber(c);
-
-console.log("Do some additions: 7 + 7");
-calculator.add(7, 7);
-console.log("Result: ", calculator.getResult());
-console.log("Now multiply the result by the saved number. ");
-console.log("Result: ", calculator.multiply(calculator.getStoredNumber()).getResult());
-
-
+};
--- index.js
+++ index.js
@@ -1,107 +1,56 @@
-
-var currentFigureShownIndex = 0;
-var numberOfFigures;
-var figures;
-var carousel;
-var directButtons;
-var intervalId;
-
-function initializeCarousel() {
-   carousel = document.getElementById("didia-carousel");
-   figures = carousel.querySelectorAll(".item");
-   directButtons = carousel.querySelectorAll("ol.carousel-indicators li");
-   numberOfFigures = figures.length;
-   attachPreviousHandler();
-   attachNextHandler();
-   attachShowAtIndexHandler();
-   showFigureAtIndex(currentFigureShownIndex);
-   startTimer();
-
-}
-
-function attachPreviousHandler() {
-    var previousButton = carousel.querySelector(".carousel-navigation.previous");
-    previousButton.onclick = function() {
-        wrapTimer(showPrevious ) ;
-    }
-}
-
-function wrapTimer( wrappedFunction ) {
-    stopTimer();
-    wrappedFunction();
-    startTimer();
-}
-
-function showPrevious() {
-    hideCurrentFigure();
-    var nextIndexToBeShown = getPreviousIndex();
-    showFigureAtIndex(nextIndexToBeShown);
-}
-
-function hideCurrentFigure() {
-    figures[currentFigureShownIndex].classList.remove("active");
+$(document).ready(function(){
+    var calculator = new Calculator();
+    var display = "";
+    var lastOperation = "";
     
-    if(currentFigureShownIndex < directButtons.length) {
-        directButtons[currentFigureShownIndex].classList.remove("active");
+    function showGeoLocation() {
+        
     }
-    
-}
-
-function getPreviousIndex() {
-    currentFigureShownIndex-- ;
-    if (currentFigureShownIndex < 0) {
-        currentFigureShownIndex += numberOfFigures;
+    function updateDisplay() {
+        return $(".display span").fadeOut('fast', function () {
+            $(this).text(display).fadeIn('fast');
+        });
     }
-    return currentFigureShownIndex;
-}
 
-function showFigureAtIndex(index) {
-    figures[index].classList.add("active");
-    if( index < directButtons.length ) {
-        directButtons[index].classList.add("active");
-    }
-}
+    $('.button-value').click(function(){
 
-function attachNextHandler() {
-    var nextButton = carousel.querySelector(".carousel-navigation.next");
-    nextButton.onclick = function() {
-        wrapTimer( showNext );
-    }
-}
+        var value = $(this).data('value');
+        display += value;
+        updateDisplay();
+    });
 
-function showNext() {
-    hideCurrentFigure();
-    var nextIndexToBeShown = getNextIndex();
-    showFigureAtIndex(nextIndexToBeShown);
-}
 
-function getNextIndex() {
-    currentFigureShownIndex  = ( currentFigureShownIndex + 1 ) % numberOfFigures;
-    return currentFigureShownIndex;
-}
-
-function attachShowAtIndexHandler() {
-    for(var i = 0; i < directButtons.length; i++) {
-        var button = directButtons[i];
-        button.onclick = function() {
-            var index = this.dataset.goTo;
-            wrapTimer( function() {
-                hideCurrentFigure();
-                currentFigureShownIndex = index;
-                showFigureAtIndex(index);
-                
-            }); 
+    $('.button-operation').click(function(){
+      
+        if (lastOperation === "+") {
+            calculator.add(display);
+        } else if (lastOperation === "-") {
+            calculator.subtract(display);
+        } else if (lastOperation === "/") {
+            calculator.divide(display);
+        } else if (lastOperation === "x") {
+            calculator.multiply(display);
+        } else if (lastOperation === "") {
+            calculator.value(display);
         }
-    }
 
-}
+        display = "";
 
-function stopTimer() {
-    clearInterval(intervalId);
-}
+        lastOperation = $(this).data('operation');
+    });
 
-function startTimer() {
-    intervalId = setInterval(showNext, 5000);
-}
+    $('.button-equals').click(function(){
+        display = calculator.equals();
+        updateDisplay();
+        lastOperation = "";
+    });
 
-initializeCarousel();
+    $('.button-clear').click(function(){
+        display = "";
+        calculator.clear();
+        updateDisplay();
+    });
+
+
+});
+
